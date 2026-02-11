8ab1431cff6e4daa296a44d2f314dc1ac731b125
Amar Pal <amarpal.i.a.s@gmail.com>
2026-02-11 19:46:28 +0530

    learnind abour doc string and ped

---
+++ Day5/docstring.py
@@ -0,0 +1,5 @@
+def square(n):
+    """This function takes a number as input and returns its square."""
+    return n ** 2
+print(square(5))
+print(square.__doc__) #to access the docstring of the function
