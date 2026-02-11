e5beb9d3e9779bfd2301b9e6c550dcf9320be795
Amar Pal <amarpal.i.a.s@gmail.com>
2026-02-11 12:21:25 +0530

    learnig list methods

---
+++ Day4/listmethod.py
@@ -0,0 +1,16 @@
+l= [12, 10, 8, 1, 2, 4, 6,4]
+print(l)
+# l.append(8) #add element at the end of the list
+# l.sort() #sort the list in ascending order
+# l.sort(reverse=True) #sort the list in descending order
+# l.reverse() #reverse the list
+# print(l.index(2)) #find the index of the first occurrence of the element
+# print(l.count(4)) #count the number of occurrences of the element
+# m = l.copy() #create a copy of the list
+# m[0] = 0
+# l.insert(2, 7) #insert element at a specific index
+m = [900, 1000, 1100]
+k=l + m #concatenate two lists
+print(k)
+# l.extend(m) #extend the list by adding elements from another list
+print(l)
