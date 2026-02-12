e5df00a150d1876ffe6e496facc5ef4f0df5435e
Amar Pal <amarpal.i.a.s@gmail.com>
2026-02-12 15:44:10 +0530

    learning about sets in python

---
+++ Day6/set.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+
+# confusion
+# amar = {} # this is not a set, this is a dictionary
+amar = set() 
+print(type(amar))
+
+for value in info:
+    print(value)     
